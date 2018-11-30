96219e9751590d23f3ed8a1b4b92d3f6aa9ec270
Sherry Bai <sb16@princeton.edu>
2018-11-30 06:31:12 +0000

    cleanup + add fuzzy signatures

--- exercises/packet_redirection/read_fp.py
+++ exercises/packet_redirection/read_fp.py
@@ -2,7 +2,7 @@
 
 import os
 import sys
-import pprint
+from copy import deepcopy
 
 # Path of fingerprint database file
 FP_PATH = 'p0f.fp'
@@ -10,10 +10,21 @@ FP_PATH = 'p0f.fp'
 MAX_DIST = 35
 
 class P0fSignature(object):
-    def __init__(self, label):
+    def __init__(self, label, label_id, match_fields, is_fuzzy=False):
         self.label = label
+        self.label_id = label_id 
 	self.is_generic = True if label.startswith('g:') else False
+        self.is_fuzzy = is_fuzzy
+        self.match_fields = match_fields
 
+    def get_match_fields_dict(self):
+        match_fields_dict = vars(self.match_fields)
+        # strip None values from dict
+        return {k: v for (k, v) in match_fields_dict.items() if v is not None}
+        
+
+class P0fRuleMatchFields(object):
+    def __init__(self):
         self.ver = None
         self.ttl = None
         self.min_ttl = None
@@ -53,6 +64,7 @@ def read_fp_file():
     # current OS label -- all signatures correspond to closest preceding OS 
     # label in the fingerprint file
     curr_label = ""
+    curr_label_id = -1
 
     # read fingerprint file line-by-line
     with open(FP_PATH) as f:
@@ -87,7 +99,6 @@ def read_fp_file():
             if line_cleaned.startswith('label'):
                 line_sep_label = line_cleaned.split('=', 1)
                 if not len(line_sep_label):
-                    # TODO: replace with error message in log
                     raise Exception('Malformed TCP SYN signature group label')
                 # store label
                 curr_label = line_sep_label[1].strip()
@@ -95,43 +106,80 @@ def read_fp_file():
                         or curr_label.startswith('s:')):
                     raise Exception('Cannot determine if signature group label'
                                     'is specific or generic')
+                curr_label_id += 1
 
             # line contains 'sys': describes expected operating systems for this
             # particular application label
             elif line_cleaned.startswith('sys'):
                 line_sep_sys = line_cleaned.split('=', 1)
                 if not len(line_sep_sys):
-                    # TODO: replace with error message in log
                     raise Exception('Malformed TCP SYN signature group sys')
                 # append to label
                 curr_label += '/{}'.format(line_sep_sys[1].strip())
 
             # line contains signature
             elif line_cleaned.startswith('sig'):
-                sig_object = process_signature(
-                    line_cleaned,
-                    curr_label
-                )
-                if sig_object:
+                (match_fields, bad_ttl) = process_match_fields(line_cleaned)
+                if match_fields:
+                    # append signature object
+                    sig_object = P0fSignature(curr_label,
+                                              curr_label_id,
+                                              match_fields)
                     signature_list.append(sig_object)
 
+                    # check if we can add a fuzzy object
+                    if (not sig_object.is_generic  # specific signature
+                        and ((match_fields.quirk_df == 1
+                              or match_fields.quirk_nz_id == 1
+                              or match_fields.quirk_zero_id == 0
+                              or match_fields.quirk_ecn == 0)  # fuzzy match quirks
+                             or not bad_ttl)):  # fuzzy match ttl
+                        fuzzy_match_fields = deepcopy(match_fields)
+
+                        # set fuzzy quirks
+                        # disappearing 'df'
+                        fuzzy_match_fields.quirk_df = \
+                            None if fuzzy_match_fields.quirk_df == 1 else 0
+                        # disappearing 'id+'
+                        fuzzy_match_fields.quirk_nz_id = \
+                            None if fuzzy_match_fields.quirk_nz_id == 1 else 0
+                        # appearing 'id-'
+                        fuzzy_match_fields.quirk_zero_id = \
+                            None if fuzzy_match_fields.quirk_zero_id == 0 else 1
+                        # appearing 'ecn'
+                        fuzzy_match_fields.quirk_ecn = \
+                            None if fuzzy_match_fields.quirk_ecn == 0 else 1
+
+                        # set fuzzy ttl
+                        fuzzy_match_fields.ttl = 255
+                        fuzzy_match_fields.min_ttl = 0
+
+                        # append fuzzy signature
+                        fuzzy_sig_object = P0fSignature(curr_label,
+                                                        curr_label_id,
+                                                        fuzzy_match_fields,
+                                                        is_fuzzy=True)
+                        signature_list.append(fuzzy_sig_object)
+
             else:
-                # TODO: replace with error message in log
                 raise Exception('Malformed line in TCP SYN section of '
                                 'fingerprint file')
 
     return signature_list
 
 
-def process_signature(line_cleaned, label):
+def process_match_fields(line_cleaned):
+    # flag for if we have encountered a "bad_ttl"
+    # (ttl ends in '-')
+    bad_ttl = False
+    
     line_sep_sig = line_cleaned.split('=', 1)
     if not len(line_sep_sig):
-        # TODO: replace with error message in log
         raise Exception('Malformed TCP SYN signature')
     
     # process signature
     sig_fields = line_sep_sig[1].strip().split(':')
-    sig_object = P0fSignature(label)
+    match_fields = P0fRuleMatchFields()
 
     # ver
     if sig_fields[0] != '*':
@@ -139,27 +187,29 @@ def process_signature(line_cleaned, label):
             # IPv6 packets are currently not supported
             # TODO: write error message to log
             return None
-        sig_object.ver = int(sig_fields[0])
+        match_fields.ver = int(sig_fields[0])
         
     # ttl
-    if sig_fields[1].endswith('-'):
-        sig_object.ttl = int(sig_fields[1].split('-')[0])
-        sig_object.min_ttl = 0
+    if sig_fields[1].endswith('-'):  # "bad_ttl"
+        bad_ttl = True
+        match_fields.ttl = int(sig_fields[1].split('-')[0])
+        match_fields.min_ttl = 0
     else:
-        sig_object.ttl = int(sig_fields[1]) 
+        ttl_operands = sig_fields[1].split('+')
+        match_fields.ttl = sum(int(op) for op in ttl_operands)
         # see p0fv3.09b/fp_tcp.c:171
-        sig_object.min_ttl = sig_object.ttl - MAX_DIST + 1
+        match_fields.min_ttl = max(0, match_fields.ttl - MAX_DIST + 1)
+
     # olen
-    sig_object.olen = int(sig_fields[2])
+    match_fields.olen = int(sig_fields[2])
     
     # mss
     if sig_fields[3] != '*':
-        sig_object.mss = int(sig_fields[3])
+        match_fields.mss = int(sig_fields[3])
 
     # split wsize and scale fields
     wsize_scale = sig_fields[4].split(',')
     if len(wsize_scale) != 2:
-        # TODO: replace with error message in log
         raise Exception('Malformed TCP SYN signature field: wsize,scale')
     
     # wsize / wsize_div_mss
@@ -172,30 +222,29 @@ def process_signature(line_cleaned, label):
             # 'mtu*N', where N is a constant
             wsize_fields = wsize.split('*')
             if len(wsize_fields) != 2:
-                # TODO: error message in log
                 raise Exception('Malformed TCP SYN signature field: wsize')
             elif wsize_fields[0] != 'mss' and wsize_fields[0] != 'mtu':
-                # TODO: error message in log
                 raise Exception('wsize cannot be a multiple of some'
                                 'value other than MSS or MTU')
             else:
                 # treat MTU as MSS 
                 # TODO: check p0f source code for how to handle MTU
-                sig_object.wsize_div_mss = int(wsize_fields[1])
+                match_fields.wsize_div_mss = int(wsize_fields[1])
         elif '%' in wsize:
             # the notation '%N', where N is a constant, is currently 
             # not supported 
             # TODO: write error message to log
-            raise Exception('Expressing wsize in the notation "%N",'
-                            'where N is a constant, is currently not'
-                            'supported')
+            # 'Expressing wsize in the notation "%N",'
+            # 'where N is a constant, is currently not'
+            # 'supported'
+            return None
         else:
             # TODO: regex match to ensure wsize can be cast to int?
-            sig_object.wsize = int(wsize)
+            match_fields.wsize = int(wsize)
         
     # scale
     if wsize_scale[1] != '*':
-        sig_object.scale = int(wsize_scale[1])
+        match_fields.scale = int(wsize_scale[1])
         
     # olayout
     olayout = 0;
@@ -205,7 +254,6 @@ def process_signature(line_cleaned, label):
         if option.startswith('eol'):
             eol_split = option.split('+', 1)
             if len(eol_split) != 2:
-                # TODO: write error message in log
                 raise Exception('End-of-line TCP option in olayout '
                                 'field must be formatted as '
                                 '"eol+n", where n represents bytes '
@@ -225,11 +273,10 @@ def process_signature(line_cleaned, label):
         elif option == 'ts':
             olayout += 8
         else:
-            # TODO: write error message in log
             raise Exception('Unknown TCP option in olayout field of '
                             'signature')
 
-    sig_object.olayout = olayout
+    match_fields.olayout = olayout
         
     # quirks
     quirk_entries = sig_fields[6].split(',')
@@ -238,49 +285,48 @@ def process_signature(line_cleaned, label):
             # no quirks
             continue
         elif quirk == 'df':
-            sig_object.quirk_df = 1
+            match_fields.quirk_df = 1
         elif quirk == 'id+':
-            sig_object.quirk_nz_id = 1
+            match_fields.quirk_nz_id = 1
         elif quirk == 'id-':
-            sig_object.quirk_zero_id = 1
+            match_fields.quirk_zero_id = 1
         elif quirk == 'ecn':
-            sig_object.quirk_ecn = 1
+            match_fields.quirk_ecn = 1
         elif quirk == '0+':
-            sig_object.quirk_nz_mbz = 1
+            match_fields.quirk_nz_mbz = 1
         elif quirk == 'flow':
             # ignore: IPv6 not supported
             continue
         elif quirk == 'seq-':
-            sig_object.quirk_zero_seq = 1
+            match_fields.quirk_zero_seq = 1
         elif quirk == 'ack+':
-            sig_object.quirk_nz_ack = 1
+            match_fields.quirk_nz_ack = 1
         elif quirk == 'ack-':
-            sig_object.quirk_zero_ack = 1
+            match_fields.quirk_zero_ack = 1
         elif quirk == 'uptr+':
-            sig_object.nz_urg = 1
+            match_fields.nz_urg = 1
         elif quirk == 'urgf+':
-            sig_object.urg = 1
+            match_fields.urg = 1
         elif quirk == 'pushf+':
-            sig_object.quirk_push = 1
+            match_fields.quirk_push = 1
         elif quirk == 'ts1-':
-            sig_object.quirk_opt_zero_ts1 = 1
+            match_fields.quirk_opt_zero_ts1 = 1
         elif quirk == 'ts2+':
-            sig_object.quirk_opt_nz_ts2 = 1
+            match_fields.quirk_opt_nz_ts2 = 1
         elif quirk == 'opt+':
-            sig_object.quirk_opt_eol_nz = 1
+            match_fields.quirk_opt_eol_nz = 1
         elif quirk == 'exws':
-            sig_object.quirk_opt_exws = 1
+            match_fields.quirk_opt_exws = 1
         elif quirk == 'bad':
-            sig_object.quirk_opt_bad = 1
+            match_fields.quirk_opt_bad = 1
         else:
-            # TODO: replace with error message in log
             raise Exception('Unknown quirk in quirks field of '
                             'fingerprint')
         
     # pclass
-    sig_object.pclass = int(sig_fields[7])
+    match_fields.pclass = int(sig_fields[7])
         
-    return sig_object
+    return (match_fields, bad_ttl)
 
 
 def main():
